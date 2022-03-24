23a2168e2ba2dac2e60e92dcdef3da9b8f60b9a4
Avinash Kumar Jha <avinjha@cisco.com>
2022-03-25 00:29:37 +0530

    first commit

---
+++ src/test/java/com/example/springbootintegration/kafkademonew/KafkademonewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootintegration.kafkademonew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkademonewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
